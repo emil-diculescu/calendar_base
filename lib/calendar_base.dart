da7dc0aa185bad67222c389ba2e20cf6bed73dd6
Emil Diculescu <emildiculescu@gmail.com>
2024-02-09 18:27:25 +0100

    Day calls builder, if provided.

--- lib/calendar_base.dart
+++ lib/calendar_base.dart
@@ -3,12 +3,56 @@ library calendar_base;
 import 'package:flutter/material.dart';
 
 @visibleForTesting
+class CalendarViewParameters extends InheritedWidget {
+  static CalendarViewParameters? maybeOf(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<CalendarViewParameters>();
+  }
+
+  static CalendarViewParameters of(BuildContext context) {
+    return maybeOf(context)!;
+  }
+
+  final Widget Function(BuildContext context, DateTime date)? dayBuilder;
+
+  // final double minMonthViewWidth;
+
+  // final double maxMonthViewWidth;
+
+  // final DateTime initialDate;
+
+  // final MaterialLocalizations localizations;
+
+  const CalendarViewParameters(
+      {super.key,
+      this.dayBuilder,
+      // required this.minMonthViewWidth,
+      // required this.maxMonthViewWidth,
+      // required this.initialDate,
+      // required this.localizations,
+      required super.child});
+
+  @override
+  bool updateShouldNotify(covariant CalendarViewParameters oldWidget) {
+    // return minMonthViewWidth != oldWidget.minMonthViewWidth ||
+    //     maxMonthViewWidth != oldWidget.maxMonthViewWidth ||
+    //     !DateUtils.isSameDay(initialDate, oldWidget.initialDate) ||
+    //     localizations != oldWidget.localizations;
+    return false;
+  }
+}
+
+@visibleForTesting
 class Day extends StatelessWidget {
   final DateTime date;
   const Day({super.key, required this.date});
 
   @override
   Widget build(BuildContext context) {
+    final builder = CalendarViewParameters.of(context).dayBuilder ?? _defaultWidget;
+    return builder(context, date);
+  }
+
+  Widget _defaultWidget(BuildContext context, DateTime date) {
     return Center(child: Text((date.day.toString())));
   }
 }
--- test/calendar_base_test.dart
+++ test/calendar_base_test.dart
@@ -1,19 +1,49 @@
 import 'package:calendar_base/calendar_base.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
+import 'calendar_base_test.mocks.dart';
+
+const _stubWidget = Placeholder();
+final MockBuilders _builders = MockBuilders();
+
+@GenerateNiceMocks([MockSpec<Builders>()])
 void main() {
   group('Day tests', () {
-    testWidgets('Displays the day by default', (widgetTester) async {
-      final date = DateUtils.dateOnly(DateTime.now());
-      await widgetTester.pumpWidget(_widgetWithDirectionality(Day(date: date)));
-      expect(find.text(date.day.toString()), findsOneWidget);
+    final DateTime testDate = _today();
+
+    setUp(() {
+      when(_builders.dayBuilder(any, any)).thenReturn(_stubWidget);
     });
 
-    testWidgets('Calls builder, if builder is provided', (widgetTester) async {});
+    testWidgets('Displays the day by default', (widgetTester) async {
+      await widgetTester.pumpWidget(_widgetWithoutBuilders(Day(date: testDate)));
+      expect(find.text(testDate.day.toString()), findsOneWidget);
+    });
+
+    testWidgets('Calls builder, if builder is provided', (widgetTester) async {
+      await widgetTester.pumpWidget(_widgetWithBuilders(Day(date: testDate)));
+      verify(_builders.dayBuilder(any, testDate));
+      expect(widgetTester.widget(find.bySubtype<Placeholder>()), _stubWidget);
+    });
   });
 }
 
-Widget _widgetWithDirectionality(Widget child) {
-  return Directionality(textDirection: TextDirection.ltr, child: child);
+abstract class Builders {
+  Widget dayBuilder(BuildContext context, DateTime date);
+}
+
+DateTime _today() {
+  return DateUtils.dateOnly(DateTime.now());
+}
+
+Widget _widgetWithoutBuilders(Widget child) {
+  return CalendarViewParameters(child: Directionality(textDirection: TextDirection.ltr, child: child));
+}
+
+Widget _widgetWithBuilders(Widget child) {
+  return CalendarViewParameters(
+      dayBuilder: _builders.dayBuilder, child: Directionality(textDirection: TextDirection.ltr, child: child));
 }
