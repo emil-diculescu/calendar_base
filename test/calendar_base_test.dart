828cf45463abf9ab98bb5c8a373475dc061f73b0
Emil Diculescu <emildiculescu@gmail.com>
2024-04-10 15:22:27 +0200

    The order of days in the weeks takes into account the locale.

--- test/calendar_base_test.dart
+++ test/calendar_base_test.dart
@@ -2,6 +2,7 @@ import 'dart:math';
 
 import 'package:calendar_base/calendar_base.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -14,18 +15,19 @@ const _minHorizontalSpacing = 16.0;
 const _minVerticalSpacing = 16.0;
 const _stubWidget = Placeholder();
 const _surfaceHeight = 600.0;
-final _builders = _configuredMockBuilders();
 final _random = Random();
 
-MockBuilders _configuredMockBuilders() {
-  final result = MockBuilders();
-  const placeholder = Text('');
-  when(result.monthNameBuilder(any, any)).thenReturn(placeholder);
-  when(result.dayBuilder(any, any)).thenReturn(placeholder);
-  when(result.monthBackgroundBuilder(any, any, any)).thenReturn(placeholder);
-  when(result.weekDayNameBuilder(any, any)).thenReturn(placeholder);
-  when(result.weekNumberBuilder(any, any)).thenReturn(placeholder);
-  return result;
+MockBuilders _builders = _ConfiguredMockBuilders();
+
+class _ConfiguredMockBuilders extends MockBuilders {
+  _ConfiguredMockBuilders() {
+    const placeholder = Text('');
+    when(monthNameBuilder(any, any)).thenReturn(placeholder);
+    when(dayBuilder(any, any)).thenReturn(placeholder);
+    when(monthBackgroundBuilder(any, any, any)).thenReturn(placeholder);
+    when(weekDayNameBuilder(any, any)).thenReturn(placeholder);
+    when(weekNumberBuilder(any, any)).thenReturn(placeholder);
+  }
 }
 
 void main() {
@@ -33,7 +35,7 @@ void main() {
     final DateTime testDate = _today();
 
     testWidgets('Displays the day by default', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(Day(date: testDate)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: Day(date: testDate)));
       expect(find.text(testDate.day.toString()), findsOneWidget);
     });
 
@@ -50,7 +52,7 @@ void main() {
     setUpAll(() => when(_builders.weekNumberBuilder(any, any)).thenReturn(_stubWidget));
 
     testWidgets('Displays week number by default', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(WeekNumber(date: testDate)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: WeekNumber(date: testDate)));
       expect(find.text(expectedWeekNumber.toString()), findsOneWidget);
     });
 
@@ -62,18 +64,22 @@ void main() {
   });
 
   group('Week day name tests', () {
-    const weekDayNames = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
-    final testIndex = _random.nextInt(weekDayNames.length);
-
-    setUpAll(() => when(_builders.weekDayNameBuilder(any, any)).thenReturn(_stubWidget));
+    final testIndex = _random.nextInt(7);
+    setUpAll(() {
+      _builders = _ConfiguredMockBuilders();
+      when(_builders.weekDayNameBuilder(any, any)).thenReturn(_stubWidget);
+    });
 
     testWidgets('Shows week day name by default', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(WeekDayName(dayIndex: testIndex)));
+      await widgetTester.pumpWidget(
+          MaterialApp(home: _CalendarTestFrameworkWithoutBuilders(child: WeekDayName(dayIndex: testIndex))));
+      final weekDayNames = _weekDayNames(widgetTester);
       expect(find.text(weekDayNames[testIndex]), findsAtLeast(1));
     });
 
     testWidgets('Calls builder, if builder is provided', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithBuilders(WeekDayName(dayIndex: testIndex)));
+      await widgetTester.pumpWidget(MaterialApp(home: _widgetWithBuilders(WeekDayName(dayIndex: testIndex))));
+      final weekDayNames = _weekDayNames(widgetTester);
       verify(_builders.weekDayNameBuilder(any, weekDayNames[testIndex]));
       expect(widgetTester.widget(find.bySubtype<Placeholder>()), _stubWidget);
     });
@@ -116,6 +122,19 @@ void main() {
       tester.testWeek(expectedRowNumber: 6, weekNumber: 27, startsWith: 30, endsWith: 30);
       tester.testNoMoreRows(expectedRowCount: 7);
     });
+
+    testWidgets('Test month with different localization', (widgetTester) async {
+      final tester = _IndividualMonthTester(widgetTester: widgetTester, year: 2024, month: 9, localeName: 'de');
+      await tester.init();
+      tester.testWeekNameRow();
+      tester.testWeek(expectedRowNumber: 1, weekNumber: 35, startsWith: 1, endsWith: 1);
+      tester.testWeek(expectedRowNumber: 2, weekNumber: 36, startsWith: 2, endsWith: 8);
+      tester.testWeek(expectedRowNumber: 3, weekNumber: 37, startsWith: 9, endsWith: 15);
+      tester.testWeek(expectedRowNumber: 4, weekNumber: 38, startsWith: 16, endsWith: 22);
+      tester.testWeek(expectedRowNumber: 5, weekNumber: 39, startsWith: 23, endsWith: 29);
+      tester.testWeek(expectedRowNumber: 6, weekNumber: 40, startsWith: 30, endsWith: 30);
+      tester.testNoMoreRows(expectedRowCount: 7);
+    });
   });
 
   group('Month name view tests', () {
@@ -124,7 +143,7 @@ void main() {
     setUpAll(() => when(_builders.monthNameBuilder(any, any)).thenReturn(_stubWidget));
 
     testWidgets('Displays the month and the year by default', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(MonthNameView(date: testDate)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: MonthNameView(date: testDate)));
       expect(find.text(_localizations.formatMonthYear(testDate)), findsOneWidget);
     });
 
@@ -139,12 +158,12 @@ void main() {
     final DateTime testDate = DateTime.now();
 
     testWidgets('Displays month name', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(MonthView(date: testDate)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: MonthView(date: testDate)));
       expect(find.bySubtype<MonthNameView>(), findsOneWidget);
     });
 
     testWidgets('Displays weeks in month', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(MonthView(date: testDate)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: MonthView(date: testDate)));
       expect(find.bySubtype<WeeksInMonthView>(), findsOneWidget);
     });
 
@@ -165,7 +184,7 @@ void main() {
   group('Calendar row testing', () {
     testWidgets('Shows one column when space is smaller than two minimum widths', (widgetTester) async {
       await _setSurfaceSize(widgetTester: widgetTester, expectedColumns: 1);
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(const CalendarRow(rowIndex: 0)));
+      await widgetTester.pumpWidget(const _CalendarTestFrameworkWithoutBuilders(child: CalendarRow(rowIndex: 0)));
       expect(find.bySubtype<MonthView>(), findsExactly(1));
     });
 
@@ -173,7 +192,7 @@ void main() {
       const maxColumns = 5;
       final expectedColumns = 2 + _random.nextInt(maxColumns - 1);
       await _setSurfaceSize(widgetTester: widgetTester, expectedColumns: expectedColumns);
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(const CalendarRow(rowIndex: 0)));
+      await widgetTester.pumpWidget(const _CalendarTestFrameworkWithoutBuilders(child: CalendarRow(rowIndex: 0)));
       expect(find.bySubtype<MonthView>(), findsExactly(expectedColumns));
     });
 
@@ -183,7 +202,7 @@ void main() {
       final testDate = DateTime.now();
       final row = 0 + _random.nextInt(5);
       final offset = row * expectedColumns;
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(CalendarRow(rowIndex: row)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: CalendarRow(rowIndex: row)));
       for (var i = 0; i < expectedColumns; ++i) {
         expect(find.text(_localizations.formatMonthYear(DateUtils.addMonthsToMonthDate(testDate, offset + i))),
             findsOneWidget);
@@ -197,7 +216,7 @@ void main() {
     testWidgets('Shows rows in correct order', (widgetTester) async {
       final expectedColumns = 1 + _random.nextInt(5);
       await _setSurfaceSize(widgetTester: widgetTester, expectedColumns: expectedColumns);
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(CalendarBase(initialDate: testDate)));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: CalendarBase(initialDate: testDate)));
       expect(find.text(_localizations.formatMonthYear(testDate)), findsOneWidget);
       final firstMonthOnSecondRow = DateUtils.addMonthsToMonthDate(testDate, expectedColumns);
       expect(find.text(_localizations.formatMonthYear(firstMonthOnSecondRow)), findsOneWidget);
@@ -247,14 +266,14 @@ void main() {
     });
 
     testWidgets('Day widgets have the date as key', (widgetTester) async {
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(CalendarBase()));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: CalendarBase()));
       expect(find.byKey(CalendarBase.dateAsKey(testDate)), findsOneWidget);
     });
 
     testWidgets('Can scroll back to current day', (widgetTester) async {
       final calendar = CalendarBase();
       final target = _localizations.formatMonthYear(DateTime.now());
-      await widgetTester.pumpWidget(_widgetWithoutBuilders(calendar));
+      await widgetTester.pumpWidget(_CalendarTestFrameworkWithoutBuilders(child: calendar));
       await widgetTester.pumpAndSettle();
       await widgetTester.fling(find.byType(CalendarBase), const Offset(0, -400), 1600);
       await widgetTester.pumpAndSettle();
@@ -273,18 +292,28 @@ class _IndividualMonthTester {
 
   final int month;
 
+  final String localeName;
+
   final List<TableRow> rows = <TableRow>[];
 
-  _IndividualMonthTester({required this.widgetTester, required this.year, required this.month});
+  _IndividualMonthTester({required this.widgetTester, required this.year, required this.month, this.localeName = 'en'});
 
   Future<void> init() async {
-    await widgetTester.pumpWidget(_widgetWithoutBuilders(WeeksInMonthView(date: DateTime(year, month, 1))));
+    await widgetTester.pumpWidget(MaterialApp(localizationsDelegates: const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate
+    ], supportedLocales: [
+      Locale(localeName)
+    ], home: _CalendarTestFrameworkWithoutBuilders(child: WeeksInMonthView(date: DateTime(year, month, 1)))));
     final widget = widgetTester.widget<Table>(find.bySubtype<Table>());
     rows.addAll(widget.children);
   }
 
   void testWeekNameRow() {
-    _testRow(rowNumber: 0, expected: ['', 'S', 'M', 'T', 'W', 'T', 'F', 'S']);
+    final expected = [''];
+    expected.addAll(_weekDayNames(widgetTester));
+    _testRow(rowNumber: 0, expected: expected);
   }
 
   void _testRow({required int rowNumber, required List<String> expected}) {
@@ -337,17 +366,6 @@ DateTime _today() {
   return DateUtils.dateOnly(DateTime.now());
 }
 
-Widget _widgetWithoutBuilders(Widget child) {
-  return CalendarViewParameters(
-      initialDate: DateTime.now(),
-      localizations: _localizations,
-      minMonthViewWidth: _minColumnWidth,
-      maxMonthViewWidth: _maxColumnWidth,
-      minHorizontalSpacing: _minHorizontalSpacing,
-      minVerticalSpacing: _minVerticalSpacing,
-      child: Directionality(textDirection: TextDirection.ltr, child: child));
-}
-
 Widget _widgetWithBuilders(Widget child) {
   return CalendarViewParameters(
       initialDate: DateTime.now(),
@@ -370,3 +388,34 @@ double _enoughWidthFor({required int columns}) {
 
 Future<void> _setSurfaceSize({required WidgetTester widgetTester, required int expectedColumns}) =>
     widgetTester.binding.setSurfaceSize(Size(_enoughWidthFor(columns: expectedColumns), _surfaceHeight));
+
+List<String> _weekDayNames(WidgetTester widgetTester) {
+  final context = widgetTester.element(find.byType(CalendarViewParameters));
+  final localizations = MaterialLocalizations.of(context);
+  final result = <String>[];
+  for (var i = localizations.firstDayOfWeekIndex; i < 7; ++i) {
+    result.add(localizations.narrowWeekdays[i]);
+  }
+  for (var i = 0; i < localizations.firstDayOfWeekIndex; ++i) {
+    result.add(localizations.narrowWeekdays[i]);
+  }
+  return result;
+}
+
+class _CalendarTestFrameworkWithoutBuilders extends StatelessWidget {
+  final Widget child;
+
+  const _CalendarTestFrameworkWithoutBuilders({required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return CalendarViewParameters(
+        initialDate: DateTime.now(),
+        localizations: Localizations.of(context, MaterialLocalizations) ?? _localizations,
+        minMonthViewWidth: _minColumnWidth,
+        maxMonthViewWidth: _maxColumnWidth,
+        minHorizontalSpacing: _minHorizontalSpacing,
+        minVerticalSpacing: _minVerticalSpacing,
+        child: Directionality(textDirection: TextDirection.ltr, child: child));
+  }
+}
